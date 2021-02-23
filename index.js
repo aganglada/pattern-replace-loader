62fa5da4448114156ea6b3224357e8cc7f331be8
Alejandro Garcia Anglada <aganglada@gmail.com>
2021-02-23 09:03:00 +0000

    fix format and remove changelog to be replaced for gh releases

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-const utils = require('loader-utils');
+const utils = require('loader-utils')
 
 function performReplacement(source, options) {
-    const searchDefined = Boolean(options.search) || options.search === '';
-    const replaceDefined = Boolean(options.replace) || options.replace === '';
-    if (searchDefined && replaceDefined) {
-        if (options.flags) {
-            options.search = new RegExp(options.search, options.flags);
-        }
+  if (!options) return source
 
-        source = source.replace(options.search, options.replace);
+  const searchDefined = Boolean(options.search) || options.search === ''
+  const replaceDefined = Boolean(options.replace) || options.replace === ''
+
+  if (searchDefined && replaceDefined) {
+    if (options.flags) {
+      options.search = new RegExp(options.search, options.flags)
     }
 
-    return source;
+    source = source.replace(options.search, options.replace)
+  }
+
+  return source
 }
 
 module.exports = function (source) {
-    if (this.cacheable) {
-        this.cacheable();
-    }
+  if (this.cacheable) {
+    this.cacheable()
+  }
 
-    const optionsConfig = this.options || this.query;
-    const options = typeof optionsConfig === 'object' ?
-        utils.getOptions(this) : utils.parseQuery(this.query);
+  const optionsConfig = this.options || this.query
+  const options =
+    typeof optionsConfig === 'object' ? utils.getOptions(this) : utils.parseQuery(this.query)
 
-    if (options && options.verbose) {
-        console.log('\nReplacing in file:', this.resourcePath);        
-    }
+  if (options && options.verbose) {
+    console.log('\nReplacing in file:', this.resourcePath)
+  }
 
-    if (Array.isArray(options.multiple)) {
-        options.multiple.forEach(function (opt) {
-            source = performReplacement(source, opt);
-        });
-    } else {
-        source = performReplacement(source, options);
-    }
+  if (Array.isArray(options.multiple)) {
+    options.multiple.forEach(function (opt) {
+      source = performReplacement(source, opt)
+    })
+  } else {
+    source = performReplacement(source, options)
+  }
 
-    return source;
-};
+  return source
+}
